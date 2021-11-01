290cec9f0a4861d870873ed9d45417fbe46089b8
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-01 18:01:33 -0400

    This code will generate infinite loop
    try to understand the code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+  if (storedUserLoggedInInformation === "1") {
+    setIsLoggedIn(true);
+  }
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1"); // localStorage is reserved name
     setIsLoggedIn(true);
   };
 
