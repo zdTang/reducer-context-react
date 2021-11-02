5d6ce29999440b90dd977df07bf85ef1e45f9d3a
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-01 21:41:07 -0400

    before using useEffect()
    By now, I am not using useEffect(),  each key stroke will run the SetFormIsValid( )
    
    too many times !

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // When the Email and Password are not valid, the Button will gray out
+  // so that whatever user inputs Email or Password
+  // Each key stroke will trigger setFromIsValid() to check if Button should be open
+
   const emailChangeHandler = (event) => {
+    console.log("in emailChangeHandler");
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    //Each key stroke will trigger this method.   too much !
+    setFormIsValid(() => {
+      console.log("in emailChangeHandler==setFormIsValid");
+      return (
+        event.target.value.includes("@") && enteredPassword.trim().length > 6
+      );
+    });
   };
 
   const passwordChangeHandler = (event) => {
+    console.log("in passwordChangeHandler");
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    //Each key stroke will trigger this method.   too much !
+    setFormIsValid(() => {
+      console.log("in passwordChangeHandler==setFormIsValid");
+      return event.target.value.trim().length > 6 && enteredEmail.includes("@");
+    });
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -45,7 +56,7 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -59,7 +70,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
