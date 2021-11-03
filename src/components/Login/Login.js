bd1b4422d51d6c977842fe85f5c994846c21de44
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-02 21:02:09 -0400

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,48 +5,48 @@ import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  console.log("login.js reloaded !");
-
   const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  // When the Email and Password are not valid, the Button will gray out
-  // so that whatever user inputs Email or Password
-  // Each key stroke will trigger setFromIsValid() to check if Button should be open
-
-  // debounce
   useEffect(() => {
-    console.log("in useEffect");
-    const identifier = setTimeout(() => {
-      // Be aware that the setTimeout() need a function definition here
-      console.log("check validition");
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
-    }, 1000);
-    // cleanup function
-    // it will run when the component un-mount from the DOM,(component is removed)
-    // Or just before next useEffect run
-    // but it will not run before the first useEffect function
-    return () => {
-      console.log("clean up");
-      clearTimeout(identifier); //   clear the last timer !!!
-    };
-  }, [enteredEmail, enteredPassword]);
+    console.log("EFFECT RUNNING");
 
-  //setFormIsValid will run only enteredEmail or enteredPassword updated
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log('Checking form validity!');
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+
+  //   return () => {
+  //     console.log('CLEANUP');
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    console.log("in emailChangeHandler");
     setEnteredEmail(event.target.value);
+
+    setFormIsValid(
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
-    console.log("in passwordChangeHandler");
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      enteredEmail.includes("@") && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
