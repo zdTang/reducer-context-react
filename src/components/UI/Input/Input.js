77b69b343481fa6f5e7f2d6776583ffb233c61eb
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-03 18:18:15 -0400

    done

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,22 +1,37 @@
-import React from "react";
-import classes from "./Input.module.css";
-export default function Input(props) {
+import React, { useRef, useImperativeHandle } from 'react';
+
+import classes from './Input.module.css';
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+
   return (
-    <>
-      <div
-        className={`${classes.control} ${
-          props.isValid === false ? classes.invalid : ""
-        }`}
-      >
-        <label htmlFor={props.id}>{props.label}</label>
-        <input
-          type={props.type}
-          id={props.id}
-          value={props.inputValue}
-          onChange={props.changeHandler}
-          onBlur={props.validateHandler}
-        />
-      </div>
-    </>
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ''
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
   );
-}
+});
+
+export default Input;
